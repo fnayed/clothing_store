219bf4558c09a37fbe3d1e5317d303dff8298b73
Andrew <andrijpastuh@gmail.com>
2024-04-18 15:13:25 +0300

    Change class Pants
    -Use the const modifier
    
    - Add Move constructor
    
    - Add binary and unary operators
    
    - Add operator =

--- Pants.cpp
+++ Pants.cpp
@@ -8,18 +8,56 @@
 using namespace std;
 
 void Pants::set_modelName(std::string modelName) {
-    this->modelName = modelName;
+    this->modelName = modelName; //приклад використання вказівника this
 }
-string Pants::get_modelName() {
-    return this->modelName;
+string Pants::get_modelName()const {
+    return this->modelName; //приклад використання вказівника this
 }
 
 void Pants::set_price(float price) {
-    this->price=price;
+    this->price=price; //приклад використання вказівника this
 }
-float Pants::get_price() {
-    return this->price;
+float Pants::get_price()const {
+    return this->price; //приклад використання вказівника this
 }
+
+
+Pants& Pants::operator=(const Pants& other) {// Оператор присвоєння =
+    if (this != &other) {
+        modelName = other.modelName;
+        price = other.price;
+    }
+    return *this;
+}
+
+Pants& Pants::operator--(){// Унарний оператор "--" (зменшити ціну)
+    if(price<=100){
+        price=0;
+    }
+    else{
+    price-=100;}
+    return *this;
+}
+
+Pants& Pants::operator++() {// Унарний оператор "++" (Збільшити ціну)
+    price+=100;
+    return *this;
+}
+
+Pants Pants::operator-(float value){// Бінарні оператор "-" (зменшити ціну)
+    if(price<=value){
+        return Pants(modelName, 0);
+    }
+    else{
+        return Pants(modelName, price - value);}
+}
+
+Pants Pants::operator+(float value) {// Бінарні оператор "+" (Збільшити ціну)
+    return Pants(modelName, price + value);
+}
+
+
+Pants::Pants(Pants&& other) noexcept : modelName(std::move(other.modelName)), price(std::move(other.price)) {} // Move constructor (Всередині)
 
 // Конструктор з параметрами за замовченням (Всередині)
 Pants::Pants(std::string modelName, float price): modelName{string(modelName)}, price{float(price)} {
--- Pants.h
+++ Pants.h
@@ -13,9 +13,19 @@ private:
     float price;
 public:
     void set_modelName(string modelName);
-    string get_modelName();
+    string get_modelName()const; //Використати модифікатор const
     void set_price(float price);
-    float get_price();
+    float get_price()const; //Використати модифікатор const
+
+    Pants& operator=(const Pants& other); // Оператор присвоєння
+
+    Pants& operator--(); // Унарний оператор "--" (зменшити ціну)
+    Pants& operator++();// Унарний оператор "++" (Збільшити ціну)
+
+    Pants operator-(float value); // Бінарні оператор "-" (зменшити ціну)
+    Pants operator+(float value);// Бінарні оператор "+" (Збільшити ціну)
+
+    Pants(Pants&& other) noexcept; // Move constructor (Обложка)
 
     Pants(string modelName = "None", float price = 0);// Конструктор з параметрами за замовченням (Оболочка)
 
